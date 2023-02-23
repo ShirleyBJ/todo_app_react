a2d69905f635165287fb69e073c5a1bc26ef1c49
Shirley Berchel <shirleyberchel@gmail.com>
2023-02-23 14:44:10 +0100

    added modal element to fill task from react modal

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import { FaPlus } from "@react-icons/all-files/fa/FaPlus";
 
-function Header() {
+function Header({addTask}) {
     return (
         <>
             <header className="flex items-end justify-between mt-3 mx-4 text-stone-600 mb-8">
                 <h1 className="text-3xl font-semibold">todo</h1>
                 <div className="text-2xl">
-                    <FaPlus/>
+                    <FaPlus onClick={() => addTask()}/>
                 </div>
             </header>
         </>
