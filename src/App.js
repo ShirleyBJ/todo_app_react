4643e25698fb3a863c4ab91d77d3b15104b50601
Shirley Berchel <shirleyberchel@gmail.com>
2023-02-24 12:10:05 +0100

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import Header from "./components/header/Header.jsx";
 import FilterBar from "./components/filterPanel/FilterBar.jsx";
 import Task from "./components/task/Task.jsx";
-import Modal from "react-modal";
 
 import './App.css';
-import { useState } from "react";
-
-Modal.setAppElement('#root');
-
 
 function App() {
-  const [modalIsOpen,setModalIsOpen] = useState(false);
-  const addTask = (e) => {
-    console.log("Task added");
-    setModalIsOpen(true);
-}
-
-  const closeModal = (e) => {
-    setModalIsOpen(false);
-  }
 
   return (
     <div className="App">
-      <Header addTask={addTask}/>
+      <Header />
       <FilterBar/>
       <Task/>
-      <Modal
-        isOpen={modalIsOpen}
-        contentLabel="Example Modal"
-      >
-        <h2>Hello</h2>
-        <button onClick={closeModal}>Close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
-      </Modal>
     </div>
-    
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import { FaPlus } from "@react-icons/all-files/fa/FaPlus";
 
-function Header({addTask}) {
+function Header() {
+    const addTask = (e) =>{
+        console.log(e);
+    }
     return (
         <>
             <header className="flex items-end justify-between mt-3 mx-4 text-stone-600 mb-8">
                 <h1 className="text-3xl font-semibold">todo</h1>
                 <div className="text-2xl">
-                    <FaPlus onClick={() => addTask()}/>
+                    <FaPlus onClick={(e) => addTask(e)}/>
                 </div>
             </header>
         </>
