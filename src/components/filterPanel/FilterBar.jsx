bbaa12bef24ed832f8d8e8a73884a80e56150377
Shirley Berchel <shirleyberchel@gmail.com>
2023-02-27 10:44:29 +0100

    added margin to filter bar

--- src/components/filterPanel/FilterBar.jsx
+++ src/components/filterPanel/FilterBar.jsx
@@ -3,7 +3,7 @@ import TagElement from "../tag/TagElement";
 function FilterBar(){
     return(
         <>
-            <section className="flex w-full justify-around">
+            <section className="flex w-full justify-around my-8">
                 <TagElement color="bg-[#D2CEFF]" tagName="work"/>
                 <TagElement color="bg-[#D1E5F7]" tagName="study"/>
                 <TagElement color="bg-[#FFCECE]" tagName="entertainement"/>
